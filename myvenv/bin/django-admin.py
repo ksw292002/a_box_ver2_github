2f9f4831e3af8e43677f81c61024fdb96145019b
SangWi Kang <ksw292002@khu.ac.kr>
2018-12-31 02:06:56 +0900

    정리(닫기)

--- myvenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Volumes/SDstorage/GoogleDriveSync/MacbookPro/workspace/django2018/myvenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
